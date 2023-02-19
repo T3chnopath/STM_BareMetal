72cf79937f50ba6552e3a549635db771871e64c9
T3chnopath <josias.navasca@gmail.com>
2023-02-19 01:52:12 -0800

    Toggled LED

--- 0_LED_Toggle/Src/main.c
+++ 0_LED_Toggle/Src/main.c
@@ -1,6 +1,6 @@
 #define PERIPH_BASE               (0x40000000UL)
 #define AHB1_PERIPH_OFFSET        (0x20000UL)
-#define AHB1_PERIPH_BASE          (PERIPH_BASE + AHB1_PERIPH_BASE)
+#define AHB1_PERIPH_BASE          (PERIPH_BASE + AHB1_PERIPH_OFFSET)
 #define GPIOA_OFFSET              (0X0000UL)
 
 #define GPIOA_BASE                (AHB1_PERIPH_BASE + GPIOA_OFFSET)
@@ -8,30 +8,36 @@
 #define RCC_OFFSET                (0x3800UL)
 #define RCC_BASE                  (AHB1_PERIPH_BASE + RCC_OFFSET)
 
-#define RCC_AHB1EN_R_OFFSET       (0x3000UL)
-#define RCC_AHB1EN_R              (*(volatile unsigned int *)(RCC_BASE + AHB1_EN_OFFSET))
+#define RCC_AHB1EN_R_OFFSET       (0x30UL)
+#define RCC_AHB1EN_R              (*(volatile unsigned int *)(RCC_BASE + RCC_AHB1EN_R_OFFSET))
 
 #define MODE_R_OFFSET             (0x0000UL)
 #define GPIOA_MODE_R              (*(volatile unsigned int*)(GPIOA_BASE + MODE_R_OFFSET))
 
-#define OD_R_OFFSET               (0x14)
+#define OD_R_OFFSET               (0x14UL)
 #define GPIOA_OD_R                (*(volatile unsigned int*)(GPIOA_BASE + OD_R_OFFSET))
 
 #define GPIOAEN                   (1U << 0) 
 
 #define PIN5                      (1U << 5)
 #define LED_PIN                   (PIN5)
-/*
-
-(1U<10) //set bit 10 to 1 
-&=~(1U<<11) //set bit 11 to 0
-
-*/
 
 int main(void) {
 
   // enable clock accesss to GPIOA
+  RCC_AHB1EN_R |= GPIOAEN;
+  
+  //set PA5 as an output
+  GPIOA_MODE_R |= (1U << 10);
+  GPIOA_MODE_R &= ~(1U << 11);
 
 
-  return 0
+  while(1) {
+      //turn on LED
+      GPIOA_OD_R ^= LED_PIN;
+
+      for(int i = 0; i < 100000; i++);
+  }
+
+  return 0;
 }
